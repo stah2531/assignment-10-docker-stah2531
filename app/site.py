b40acd72d90322033e9d79241c9dd013d08965cc
stah2531 <42522927+stah2531@users.noreply.github.com>
2018-11-19 22:21:57 -0600

    Update site.py

--- app/site.py
+++ app/site.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 def root_page():
     return ('<html><body><b>Working with containers is super fun!</b>' +
-        '</body></html>')
+            '</body></html>')
 
 
 if __name__ == '__main__':
